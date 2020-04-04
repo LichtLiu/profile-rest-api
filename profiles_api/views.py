a10f6f9d6020056ffe18e97bc17b1f1393ec99ec
LYX <joy9517538246@gmail.com>
2020-04-04 09:05:14 +0800

    Added Login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -102,6 +104,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    """Handle creating User Authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 
